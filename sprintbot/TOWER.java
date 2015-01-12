799b858705b9fe1be31523738e6f9d1369df41e1
David Marne <davemarne@gmail.com>
2015-01-11 21:51:08 -0700

    fix ant failures

--- sprintbot/TOWER.java
+++ sprintbot/TOWER.java
@@ -4,23 +4,24 @@ import battlecode.common.*;
 
 public class TOWER {
     public static RobotController rc;
-    public static int prevHealth = 1000;
+    public static double prevHealth = 1000;
 
     public static void execute(RobotController rc_in) throws GameActionException {
         rc = rc_in;
         RobotInfo[] enemyRobots = rc.senseNearbyRobots(999999, RobotPlayer.enemyTeam);
-        int currHealth = rc.getHealth();
+        double currHealth = rc.getHealth();
         if (currHealth < prevHealth) {
-            rc.broadCast(TOWER_UNDER_DISTRESS + getTowerOrdinal(), 1);
+            rc.broadcast(MyConstants.TOWER_UNDER_DISTRESS + getTowerOrdinal(), 1);
         } else {
-            rc.broadCast(TOWER_UNDER_DISTRESS + getTowerOrdinal(), 0);
+            rc.broadcast(MyConstants.TOWER_UNDER_DISTRESS + getTowerOrdinal(), 0);
         }
         Util.attack(rc, enemyRobots);
         prevHealth = rc.getHealth();
     }
 
     public static int getTowerOrdinal() {
-        for (int i = 0; i < RobotPlayer.myTowers; i++) {
+        MapLocation myLocation = rc.getLocation();
+        for (int i = 0; i < RobotPlayer.myTowers.length; i++) {
             if (myLocation.equals(RobotPlayer.myTowers[i])) {
                 return i;
             }
